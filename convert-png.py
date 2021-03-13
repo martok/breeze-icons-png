69aa34f42ffaa4910e4a7f7c1fd6bd6416bc8289
Martok <martok@martoks-place.de>
2021-03-13 03:15:11 +0100

    add converter script

---
+++ convert-png.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+from typing import Optional
+from tqdm import tqdm
+
+NUM_PROCS=4
+INKSCAPE="inkscape"
+ICON_CLASSES=[ "icons", "icons-dark" ]
+ICON_SIZES=[ 16, 22, 32, 48, 64, 96]
+
+PATH_SRC=Path("./breeze-icons")
+PATH_PNG=Path("./png")
+
+
+def get_png_name(p: Path) -> Path:
+     *_, cls, cat, size, name = p.with_suffix("").parts
+     return PATH_PNG  / cls / f"{cat}-{name}_{size}.png"
+
+def parallel_execute(commands, np: Optional[int] = None):
+    if np == 1:
+        for command in tqdm(commands):
+            run_command(command)
+    else:
+        from multiprocessing.pool import ThreadPool
+        import subprocess
+
+        def run_command(cmd):
+            try:
+                res = subprocess.run(cmd)
+                return res.returncode
+            except Exception as e:
+                print(e)
+                return 0
+
+        tp = ThreadPool(np)
+        gen = tp.imap_unordered(run_command, commands, chunksize=1)
+        for r in tqdm(gen, total=len(commands), smoothing=0.1):
+            pass
+        tp.close()
+        tp.join()
+
+
+if __name__ == "__main__":
+    svg_files = []
+    for cls in ICON_CLASSES:
+        pclass = PATH_PNG / cls
+        pclass.mkdir(parents=True, exist_ok=True)
+        sclass = PATH_SRC / cls
+        for size in ICON_SIZES:
+            svg_files += sclass.glob(f"*/{size}/*.svg")
+    png_files = [get_png_name(f) for f in svg_files]
+    commands = []
+    for svg, png in zip(svg_files, png_files):
+        sstat = svg.stat()
+        if sstat.st_size <= 50:
+            continue
+        with svg.open("rt") as t:
+            if "<svg" not in t.read(500):
+                continue
+        if not png.exists() or (png.stat().st_mtime < sstat.st_mtime):
+            commands.append((INKSCAPE, "-z",  "-b", "#FF00FF", "-y", "0", str(svg), "-e", str(png)))
+    print(len(commands), " new conversions found")
+    if len(commands):
+        parallel_execute(commands, NUM_PROCS)
